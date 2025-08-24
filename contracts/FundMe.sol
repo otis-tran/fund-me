3085822e5353a23c96d661c4ab3558d2a5d3b722
otis-tran <otis.tran2000@gmail.com>
2025-08-24 22:48:51 +0700

    Implement Chainlink Data Feeds

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,13 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 /**
  * @title FundMe
  * @dev Hợp đồng crowdfunding phi tập trung
  * @notice Cho phép người dùng gửi ETH và owner rút tiền
  */
 contract FundMe {
-    
+    AggregatorV3Interface internal dataFeed;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: BTC/USD
+     * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+     */
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+    }
+
+    /**
+     * Returns the latest answer.
+     */
+    function getPrice() public view returns (int256) {
+        // prettier-ignore
+        (
+            /* uint80 roundId */,
+            int256 answer,
+            /*uint256 startedAt*/,
+            /*uint256 updatedAt*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return int256(answer) * 1e10;
+    }
+
+    function getDecimals() public view returns (uint8) {
+        return dataFeed.decimals();
+    }
+
     /**
      * @dev Hàm nhận tiền từ người dùng
      * @notice Gửi ETH vào hợp đồng để ủng hộ dự án
@@ -16,7 +49,7 @@ contract FundMe {
         // Kiểm tra số lượng ETH tối thiểu (1 ETH = 1e18 Wei)
         require(msg.value > 1e18, "You need to spend more ETH!");
     }
-    
+
     /**
      * @dev Lấy số dư hiện tại của contract
      */
